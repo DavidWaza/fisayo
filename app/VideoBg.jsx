fdbf46ed7eefa6f03cd4e768afcad883517260d3
david waza <idemetoemediong2022@gmail.com>
2023-09-21 17:15:07 +0100

    responsive hero text

--- app/VideoBg.jsx
+++ app/VideoBg.jsx
@@ -20,16 +20,16 @@ const VideoBg = () => {
         <div className="overlay"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="w-full">
-            <h1 className=" text-2xl text-white uppercase dosis tracking-widest text-center">
+            <h1 className=" text-[1rem] md:text-2xl text-white uppercase dosis tracking-widest text-center mb-5 md:mb-0">
               Without #FODaADU, i&apos;m
             </h1>
-            <p className="uppercase text-[114px] w-full din font-bold text-[#B5AB99]">
+            <p className="uppercase text-[2.3rem] md:text-[114px] w-full din font-bold text-[#B5AB99] leading-[40px] md:leading-[100px] lg:leading-[170px] mb-5 md:mb-0">
               Fisayo Fosudo
             </p>
-            <p className="text-white uppercase text-[2rem] w-full mb-5 din-light tracking-wider">
+            <p className="text-white uppercase text-[1rem] md:text-[2rem] w-full mb-5 din-light tracking-wider">
               Visual storyteller & tech video producer
             </p>
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-[3rem]">
               <button className="text-white p-4 px-10 rounded-none dosis bg-[#FF0000] flex items-center hover:rounded-full transform transition-transform duration-300 ease-in-out hover:opacity-80 hover:scale-110">
                 <Link
                   href="https://www.youtube.com/results?search_query=fisayo+fosudo"
