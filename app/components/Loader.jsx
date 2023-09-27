ba30e78a6a76523ef4f4c8106d9e3c3b191a526f
david waza <idemetoemediong2022@gmail.com>
2023-09-27 16:02:01 +0100

    added a loading animation

---
+++ app/components/Loader.jsx
@@ -0,0 +1,17 @@
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+export default Loader;
--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
-import Image from 'next/image'
-import VideoBg from './components/VideoBg'
-import WorkPortfolio from './components/Work'
-import Footer from './components/Footer'
-import EmailSection from './components/EmailSection'
+'use client'
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import VideoBg from "./components/VideoBg";
+import WorkPortfolio from "./components/Work";
+import Footer from "./components/Footer";
+import EmailSection from "./components/EmailSection";
+import Loader from "./components/Loader";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(true), 6000);
+  }, []);
   return (
-    <main className="">
-      <VideoBg />
-      <WorkPortfolio />
-      <EmailSection />
-      <Footer />
+    <main className="bg-transparent">
+      {isLoading ? (
+        <>
+          <VideoBg />
+          <WorkPortfolio />
+          <EmailSection />
+          <Footer />
+        </>
+      ) : (
+        <Loader />
+      )}
     </main>
-  )
+  );
 }
